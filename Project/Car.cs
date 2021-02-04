1a8fe0d3179e4508f3168e1fd5530a84df68ac47
CianTivnan <cian.tivnan@yahoo.ie>
2021-02-04 12:30:24 +0000

    In process of fixing lbx_Cars null error

---
+++ Project/Car.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    abstract class Car
+    {
+        //attributes
+        public string Name { get; set; }
+
+        public int TopSpeed { get; set; }
+
+        public double ZeroTo100 { get; set; }
+
+        public int Horsepower { get; set; }
+
+        public int Torque { get; set; }
+
+        public int MaxRpm { get; set; }
+
+        public int FuelMpg { get; set; }
+
+        //ctors
+        public Car(){}
+
+        public Car(string name, int topSpeed, double zeroTo100, int horsePower, int torque, int maxRpm, int mpg)
+        {
+            Name = name;
+            TopSpeed = topSpeed;
+            ZeroTo100 = zeroTo100;
+            Horsepower = horsePower;
+            Torque = torque;
+            MaxRpm = maxRpm;
+            FuelMpg = mpg;
+        }
+    }
+
+    class Coupe : Car
+    {
+        //ctor
+        public Coupe(string name, int topSpeed, double zeroTo100, int horsePower, int torque, int maxRpm, int mpg) : base(name, topSpeed, zeroTo100, horsePower, torque, maxRpm, mpg)
+        {}
+    }
+
+    class Hatchback : Car
+    {
+        //ctor
+        public Hatchback(string name, int topSpeed, double zeroTo100, int horsePower, int torque, int maxRpm, int mpg) : base(name, topSpeed, zeroTo100, horsePower, torque, maxRpm, mpg)
+        { }
+    }
+
+    class Saloon : Car
+    {
+        //ctor
+        public Saloon(string name, int topSpeed, double zeroTo100, int horsePower, int torque, int maxRpm, int mpg) : base(name, topSpeed, zeroTo100, horsePower, torque, maxRpm, mpg)
+        { }
+    }
+
+    class Estate : Car
+    {
+        //ctor
+        public Estate(string name, int topSpeed, double zeroTo100, int horsePower, int torque, int maxRpm, int mpg) : base(name, topSpeed, zeroTo100, horsePower, torque, maxRpm, mpg)
+        { }
+    }
+}
--- Project/MainWindow.xaml.cs
+++ Project/MainWindow.xaml.cs
@@ -15,14 +15,123 @@ using System.Windows.Shapes;
 
 namespace Project
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    /// LARGE ERROR PRESENT
+    /// LOGIC ERROR - LBX_NULL ON LOAD
+    /// MUST AVOID PROGRAM FROM JUMPING INTO SELECTION BOX CHANGED ON LOAD
+    /// 
     public partial class MainWindow : Window
     {
+        static List<Car> CarList = new List<Car>();
+        static List<Car> DisplayList = new List<Car>();
+
         public MainWindow()
         {
+            
+
             InitializeComponent();
+        }
+
+        public void LoadCars()
+        {
+            Coupe MazdaMx5 = new Coupe("Mazda MX-5", 191, 9.7, 108, 134, 7200, 35);
+            CarList.Add(MazdaMx5);
+            Hatchback CivicTR = new Hatchback("Honda Civic Type-R", 270, 5.7, 306, 400, 8000, 39);
+            CarList.Add(CivicTR);
+
+            if (lbx_Cars.ItemsSource != null)
+                lbx_Cars.ItemsSource = null;
+
+            ComboBoxItem item = (ComboBoxItem)cbx_CarClass.SelectedItem;
+            string selection = item.Content.ToString();
+
+            switch (selection)
+            {
+                case "All":
+                    foreach (Car car in CarList)
+                    {
+                        DisplayList.Add(car);
+                    }
+                    break;
+                case "Coupe":
+                    foreach (Coupe car in CarList)
+                    {
+                        DisplayList.Add(car);
+                    }
+                    break;
+                case "Hatchback":
+                    foreach (Hatchback car in CarList)
+                    {
+                        DisplayList.Add(car);
+                    }
+                    break;
+                case "Saloon":
+                    foreach (Saloon car in CarList)
+                    {
+                        DisplayList.Add(car);
+                    }
+                    break;
+                case "Estate":
+                    foreach (Estate car in CarList)
+                    {
+                        DisplayList.Add(car);
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            lbx_Cars.ItemsSource = DisplayList; 
+        }
+
+        private void cbx_CarClass_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+         
+
+            ComboBoxItem item = (ComboBoxItem)cbx_CarClass.SelectedItem;
+            string selection = item.Content.ToString();
+
+            switch(selection)
+            {
+                case "All":
+                    foreach (Car car in CarList)
+                    {
+                        DisplayList.Add(car);
+                    }
+                    break;
+                case "Coupe":
+                    foreach (Coupe car in CarList)
+                    {
+                        DisplayList.Add(car);
+                    }
+                    break;
+                case "Hatchback":
+                    foreach (Hatchback car in CarList)
+                    {
+                        DisplayList.Add(car);
+                    }
+                    break;
+                case "Saloon":
+                    foreach (Saloon car in CarList)
+                    {
+                        DisplayList.Add(car);
+                    }
+                    break;
+                case "Estate":
+                    foreach (Estate car in CarList)
+                    {
+                        DisplayList.Add(car);
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            lbx_Cars.ItemsSource = DisplayList;
+        }
+
+        private void lbx_Cars_Initialized(object sender, EventArgs e)
+        {
+            LoadCars();
         }
     }
 }
